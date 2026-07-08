f624b07d5ea7086de3acc8d52d662b1b4624999f
sorowagniguizelika <sorozelika@gmail.com>
2026-07-08 14:49:59 +0000

    modificatio du comportement
    Signed-off-by: sorowagniguizelika <sorozelika-ui>

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
 //import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
@@ -24,46 +28,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    /*Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText("Happy BIRTHDAY ZHOULEYKHA!", "FROM EMMA")
 
-                     */
                 }
             }
         }
     }
 }
-// suprimer de greeting dans tout le code changer en GreetingText
 
-/*@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
- */
 
 @Composable
 // parametre modifier de type Modifier et la valeur par default = Modifier
 
 fun GreetingText(message : String , from : String , modifier: Modifier = Modifier){
     //AJOUT DU COMPOSANT TEXT
-    Column {
+    Column(verticalArrangement = Arrangement.Center, modifier = modifier.padding(8.dp)) {
         Text(
             text = message,
             color = Color.Magenta,
-            fontSize = 100.sp,
-            lineHeight = 116.sp
+            fontSize = 56.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
             fontSize = 36.sp,
-            color = Color.White
+            color = Color.Black
         )
     }
 }
@@ -72,9 +66,8 @@ fun GreetingText(message : String , from : String , modifier: Modifier = Modifie
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-       // Greeting("zhouleykha")
-        // appel de la function GreetingText
-        GreetingText("Happy BIRTHDAY ZHOULEYKHA!","FROM EMMA")
+
+        GreetingText("Happy BIRTHDAY ZHOULEYKHA!", "FROM EMMA")
 
     }
 }
